320d448667540090ed66a55e75a6f2efcb8bc7fe
apkille <killeandrew@gmail.com>
2024-09-13 21:06:07 -0400

    makie convert typo fix

--- ext/MakieExt/MakieExt.jl
+++ ext/MakieExt/MakieExt.jl
@@ -4,7 +4,7 @@ using Makie
 using Gabs
 
 function Makie.convert_arguments(P::Type{<:Makie.Heatmap}, q, p, s::GaussianState)
-    !isequal(length(s.mean),2) || throw(ArgumentError(Gabs.MAKIE_ERROR))
+    isequal(length(s.mean),2) || throw(ArgumentError(Gabs.MAKIE_ERROR))
     wigner_data = [wigner(s, [i,j]) for i in q, j in p]
     Makie.convert_arguments(P, q, p, wigner_data)
 end
