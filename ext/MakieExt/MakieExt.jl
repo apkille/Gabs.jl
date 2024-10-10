459d98d99007a8045ae0acdce2baa4e0fd7cc390
apkille <killeandrew@gmail.com>
2024-10-09 23:03:48 -0400

    add Makie heatmap attributes

--- ext/MakieExt/MakieExt.jl
+++ ext/MakieExt/MakieExt.jl
@@ -1,12 +1,21 @@
 module MakieExt
 
 using Makie
-using Gabs
+using Gabs: GaussianState, wigner, wignerchar
 
-function Makie.convert_arguments(P::Type{<:Makie.Heatmap}, q, p, s::GaussianState)
-    isequal(length(s.mean),2) || throw(ArgumentError(Gabs.MAKIE_ERROR))
-    wigner_data = [wigner(s, [i,j]) for i in q, j in p]
-    Makie.convert_arguments(P, q, p, wigner_data)
+Makie.used_attributes(::Type{<:Makie.Heatmap}, ::Any, ::Any, x::GaussianState) = (:dist,)
+
+# Directly use heatmap to create wigner distribution
+function Makie.convert_arguments(P::Type{<:Makie.Heatmap}, q, p, state::GaussianState; dist = :wigner)
+    isequal(length(state.mean),2) || throw(ArgumentError(Gabs.MAKIE_ERROR))
+    if isequal(dist, :wigner)
+        data = [wigner(state, [i,j]) for i in q, j in p]
+    elseif isequal(dist, :wignerchar)
+        data = [real(wignerchar(state, [i,j])) for i in q, j in p]
+    else
+        throw(ErrorException("`distribution` should be `:wigner` or `:wignerchar`"))
+    end
+    return convert_arguments(P, q, p, data)
 end
 
 end
