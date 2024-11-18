6a02a85d0d0c202e773d831dfe4adfc95afce656
Andrew Kille <68079167+apkille@users.noreply.github.com>
2024-11-17 20:56:50 -0500

    Generate valid Gaussian states, unitaries, and channels (#10)
    * add random features
    
    * add more tests
    
    * fix
    
    * change basis of symplecticform
    
    * update project.toml and CHANGELOG
    
    * alter Float32 test
    
    * add codecov for `randsymplectic`
    
    * change floating point tests for rand objects

--- src/randoms.jl
+++ src/randoms.jl
@@ -1,22 +1,53 @@
 """
-    randstate([Tm=Vector{Float64}, Tc=Matrix{Float64},] nmodes<:Int)
+    randstate([Tm=Vector{Float64}, Tc=Matrix{Float64},] nmodes<:Int; pure=false)
 
 Calculate a random Gaussian state.
 """
-function randstate(::Type{Tm}, ::Type{Tc}, nmodes::N) where {Tm,Tc,N<:Int}
-    mean = randn(2*nmodes)
-    randmat = randn(2*nmodes, 2*nmodes)
-    # positive-definite condition
-    covar = randmat' * randmat
+function randstate(::Type{Tm}, ::Type{Tc}, nmodes::N; pure = false) where {Tm,Tc,N<:Int}
+    mean, covar = _randstate_fields(nmodes, pure)
     return GaussianState(Tm(mean), Tc(covar), nmodes)
 end
-randstate(::Type{T}, nmodes::N) where {T,N<:Int} = randstate(T,T,nmodes)
-function randstate(nmodes::N) where {N<:Int}
-    mean = randn(2*nmodes)
-    randmat = randn(2*nmodes, 2*nmodes)
-    # positive-definite condition
-    covar = randmat' * randmat
+randstate(::Type{T}, nmodes::N; pure = false) where {T,N<:Int} = randstate(T,T,nmodes,pure=pure)
+function randstate(nmodes::N; pure = false) where {N<:Int}
+    mean, covar = _randstate_fields(nmodes, pure)
     return GaussianState(mean, covar, nmodes)
+end
+function _randstate_fields(nmodes::N, pure) where {N<:Int}
+    mean = randn(2*nmodes)
+    covar = zeros(2*nmodes, 2*nmodes)
+    symp = randsymplectic(nmodes)
+    # generate pure Gaussian state
+    if pure
+        mul!(covar, symp, symp')
+        return mean, covar
+    end
+    # create buffer for matrix multiplication
+    buf = zeros(2*nmodes, 2*nmodes)
+    # William decomposition for mixed Gaussian states
+    sympeigs = abs.(rand(nmodes)) .+ 1.0
+    will = diagm(repeat(sympeigs, inner = 2))
+    mul!(covar, symp, mul!(buf, will, symp'))
+    return mean, covar
+end
+
+"""
+    randunitary([Td=Vector{Float64}, Ts=Matrix{Float64},] nmodes<:Int; passive=false)
+
+Calculate a random Gaussian unitary operator.
+"""
+function randunitary(::Type{Td}, ::Type{Ts}, nmodes::N; passive = false) where {Td,Ts,N}
+    disp, symp = _randunitary_fields(nmodes, passive)
+    return GaussianUnitary(Td(disp), Ts(symp))
+end
+randunitary(::Type{T}, nmodes::N; passive = false) where {T,N<:Int} = randunitary(T,T,nmodes; passive = passive)
+function randunitary(nmodes::N; passive = false) where {N<:Int}
+    disp, symp = _randunitary_fields(nmodes, passive)
+    return GaussianUnitary(disp, symp)
+end
+function _randunitary_fields(nmodes::N, passive) where {N<:Int}
+    disp = rand(2*nmodes)
+    symp = randsymplectic(nmodes, passive = passive)
+    return disp, symp
 end
 
 """
@@ -25,19 +56,74 @@ end
 Calculate a random Gaussian channel.
 """
 function randchannel(::Type{Td}, ::Type{Tt}, nmodes::N) where {Td,Tt,N<:Int}
-    disp = randn(2*nmodes)
-    transform = randn(2*nmodes, 2*nmodes)
-    randmat = randn(2*nmodes, 2*nmodes)
-    # positive-definite condition
-    noise = randmat' * randmat
+    disp, transform, noise = _randchannel(nmodes)
     return GaussianChannel(Td(disp), Tt(transform), Tt(noise), nmodes)
 end
 randchannel(::Type{T}, nmodes::N) where {T,N<:Int} = randchannel(T,T,nmodes)
 function randchannel(nmodes::N) where {N<:Int}
-    disp = randn(2*nmodes)
-    transform = randn(2*nmodes, 2*nmodes)
-    randmat = randn(2*nmodes, 2*nmodes)
-    # positive-definite condition
-    noise = randmat' * randmat
+    disp, transform, noise = _randchannel(nmodes)
     return GaussianChannel(disp, transform, noise, nmodes)
+end
+function _randchannel(nmodes::N) where {N<:Int}
+    disp = randn(2*nmodes)
+    # generate symplectic matrix describing the evolution of the system with N modes
+    # and environment with 2N modes
+    symp = randsymplectic(3*nmodes)
+    transform, B = symp[1:2*nmodes, 1:2*nmodes], @view(symp[1:2*nmodes, 2*nmodes+1:6*nmodes])
+    # generate noise matrix from evolution of environment
+    noise = zeros(2*nmodes, 2*nmodes)
+    mul!(noise, B, B')
+    return disp, transform, noise
+end
+
+"""
+    randsymplectic([T=Matrix{Float64},] nmodes<:Int, passive=false)
+
+Calculate a random symplectic matrix of size `2*nmodes x 2*nmodes`.
+"""
+function randsymplectic(nmodes::N; passive = false) where {N<:Int}
+    # generate random orthogonal symplectic matrix
+    O = _rand_orthogonal_symplectic(nmodes)
+    if passive
+        return O
+    end
+    # instead generate random active symplectic matrix
+    O′ = _rand_orthogonal_symplectic(nmodes)
+    # direct sum of symplectic matrices for single-mode squeeze transformations
+    rs = rand(nmodes)
+    squeezes = zeros(2*nmodes, 2*nmodes)
+    @inbounds for i in Base.OneTo(nmodes)
+        val = rs[i]
+        squeezes[2*i-1, 2*i-1] = val
+        squeezes[2*i, 2*i] = 1/val
+    end
+    return O * squeezes * O′
+end
+function randsymplectic(::Type{T}, nmodes::N; passive = false) where {T, N<:Int} 
+    symp = randsymplectic(nmodes, passive = passive)
+    return T(symp)
+end
+
+# Generates random orthogonal symplectic matrix by blocking real
+# and imaginary parts of a random unitary matrix
+function _rand_orthogonal_symplectic(nmodes::N) where {N<:Int}
+    U = _rand_unitary(nmodes)
+    O = zeros(2*nmodes, 2*nmodes)
+    @inbounds for i in Base.OneTo(nmodes), j in Base.OneTo(nmodes)
+        val = U[i,j]
+        O[2*i-1,2*j-1] = real(val)
+        O[2*i, 2*j-1] = -imag(val)
+        O[2*i-1, 2*j] = imag(val)
+        O[2*i, 2*j] = real(val)
+    end
+    return O
+end
+# Generates unitary matrix randomly distributed over Haar measure;
+# see https://arxiv.org/abs/math-ph/0609050 for algorithm.
+# This approach is faster and creates less allocations than rand(Haar(2), size) from RandomMatrices.jl
+function _rand_unitary(size::N) where {N<:Int}
+    M = rand(ComplexF64, size, size) ./ sqrt(2.0)
+    q, r = qr(M)
+    d = diagm([r[i, i] / abs(r[i, i]) for i in Base.OneTo(size)])
+    return q * d
 end
--- test/test_randoms.jl
+++ test/test_randoms.jl
@@ -1,22 +1,98 @@
 @testitem "Measurements" begin
     using Gabs
     using StaticArrays
+    using LinearAlgebra: eigvals, adjoint
 
-    @testset "random types" begin
+    @testset "random utils" begin
+        nmodes = rand(1:20)
+        U = Gabs._rand_unitary(nmodes)
+        @test isapprox(adjoint(U), inv(U), atol = 1e-5)
+
+        O = Gabs._rand_orthogonal_symplectic(nmodes)
+        @test isapprox(O', inv(O), atol = 1e-5)
+        Omega = symplecticform(nmodes)
+        @test isapprox(O' * Omega * O, Omega, atol = 1e-5)
+
+        Spassive = randsymplectic(nmodes, passive = true)
+        @test isapprox(Spassive', inv(Spassive), atol = 1e-5)
+        @test isapprox(Spassive' * Omega * Spassive, Omega, atol = 1e-5)
+
+        S = randsymplectic(nmodes)
+        @test isapprox(S' * Omega * S, Omega, atol = 1e-5)
+
+        S_array = randsymplectic(Array, nmodes)
+        @test isapprox(S_array' * Omega * S_array, Omega, atol = 1e-5)
+    end
+
+    @testset "random states" begin
         nmodes = rand(1:20)
         rs = randstate(nmodes)
         rc = randchannel(nmodes)
         @test rc isa GaussianChannel
         @test rc * rs isa GaussianState
+        Omega = symplecticform(nmodes)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(rs.covar .+ im*Omega)))
 
-        rsfloat32 = randstate(AbstractArray{Float32}, nmodes)
-        rcfloat32 = randchannel(AbstractArray{Float32}, nmodes)
-        @test rcfloat32 isa GaussianChannel
-        @test rcfloat32 * rsfloat32 isa GaussianState
+        rspure = randstate(nmodes, pure = true)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(rspure.covar .+ im*Omega)))
+        @test isapprox(purity(rspure), 1.0, atol = 1e-5)
 
-        rsstatic = randstate(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes)
-        rcstatic = randchannel(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes)
-        @test rcstatic isa GaussianChannel
-        @test rcstatic * rsstatic isa GaussianState
+        rs_array = randstate(Array, nmodes)
+        rc_array = randchannel(Array, nmodes)
+        @test rc_array isa GaussianChannel
+        @test rc_array * rs_array isa GaussianState
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(rs_array.covar .+ im*Omega)))
+
+        rspure_array = randstate(Array, nmodes, pure = true)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-3)), real(eigvals(rspure_array.covar .+ im*Omega)))
+        @test isapprox(purity(rspure_array), 1.0, atol = 1e-3)
+
+        rs_static = randstate(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes)
+        rc_static = randchannel(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes)
+        @test rc_static isa GaussianChannel
+        @test rc_static * rs_static isa GaussianState
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(Array(rs_static.covar) .+ im*Omega)))
+
+        rspure_static = randstate(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes, pure = true)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(Array(rspure_static.covar) .+ im*Omega)))
+        @test isapprox(purity(rspure_static), 1.0, atol = 1e-5)
+    end
+
+    @testset "random unitaries" begin
+        nmodes = rand(1:20)
+        ru = randunitary(nmodes)
+        Omega = symplecticform(nmodes)
+        @test isapprox(ru.symplectic' * Omega * ru.symplectic, Omega, atol = 1e-5)
+
+        rupassive = randunitary(nmodes, passive = true)
+        @test isapprox(rupassive.symplectic', inv(rupassive.symplectic), atol = 1e-5)
+        @test isapprox(rupassive.symplectic' * Omega * rupassive.symplectic, Omega, atol = 1e-5)
+
+        ru_array = randunitary(Array, nmodes)
+        @test isapprox(ru_array.symplectic' * Omega * ru_array.symplectic, Omega, atol = 1e-5)
+
+        rupassive_array = randunitary(nmodes, passive = true)
+        @test isapprox(rupassive_array.symplectic', inv(rupassive_array.symplectic), atol = 1e-5)
+        @test isapprox(rupassive_array.symplectic' * Omega * rupassive_array.symplectic, Omega, atol = 1e-5)
+
+        ru_static = randunitary(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes)
+        @test isapprox(ru_static.symplectic' * Omega * ru_static.symplectic, Omega, atol = 1e-5)
+
+        rupassive_static = randunitary(SVector{2*nmodes}, SMatrix{2*nmodes,2*nmodes}, nmodes, passive = true)
+        @test isapprox(rupassive_static.symplectic', inv(rupassive_static.symplectic), atol = 1e-5)
+        @test isapprox(rupassive_static.symplectic' * Omega * rupassive_static.symplectic, Omega, atol = 1e-5)
+    end
+
+    @testset "random channels" begin
+        nmodes = rand(1:20);
+        rc = randchannel(nmodes);
+        Omega = symplecticform(nmodes);
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(rc.noise .+ im*Omega .- im*rc.transform*Omega*rc.transform')))
+
+        rc_array = randchannel(Array, nmodes)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(rc_array.noise .+ im*Omega .- im*rc_array.transform*Omega*rc_array.transform')))
+
+        rc_static = randchannel(SVector{2*nmodes}, SMatrix{2*nmodes, 2*nmodes}, nmodes)
+        @test all(i -> ((i >= 0) || isapprox(i, 0.0, atol = 1e-5)), real(eigvals(Array(rc_static.noise) .+ im*Omega .- im*Array(rc_static.transform)*Omega*Array(rc_static.transform)')))
     end
 end
