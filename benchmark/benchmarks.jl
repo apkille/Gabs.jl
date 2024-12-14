36e66ed89f9b4546b694e2e26b8cecec4e309489
apkille <killeandrew@gmail.com>
2024-12-14 05:40:47 -0500

    update benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -16,8 +16,9 @@ for op in op_list
 end
 
 for nmodes in (2, 10, 50, 100, 200)
-    SUITE["operations"]["unitary product"][string(nmodes)] = @benchmarkable op * state setup=(op = randunitary($nmodes); state = randstate($nmodes))
-    SUITE["operations"]["channel product"][string(nmodes)] = @benchmarkable ch * state setup=(ch = randchannel($nmodes); state = randstate($nmodes))
-    SUITE["operations"]["tensor product"][string(nmodes)] = @benchmarkable state1 ⊗ state2 setup=(state1 = randstate($nmodes); state2 = randstate($nmodes))
-    SUITE["operations"]["partial trace"][string(nmodes)] = @benchmarkable ptrace(state, indices) setup=(state = randstate($nmodes); indices = collect(2:$nmodes))
+    basis = QuadPairBasis(nmodes)
+    SUITE["operations"]["unitary product"][string(nmodes)] = @benchmarkable op * state setup=(op = randunitary($basis); state = randstate($basis))
+    SUITE["operations"]["channel product"][string(nmodes)] = @benchmarkable ch * state setup=(ch = randchannel($basis); state = randstate($basis))
+    SUITE["operations"]["tensor product"][string(nmodes)] = @benchmarkable state1 ⊗ state2 setup=(state1 = randstate($basis); state2 = randstate($basis))
+    SUITE["operations"]["partial trace"][string(nmodes)] = @benchmarkable ptrace(state, indices) setup=(state = randstate($basis); indices = collect(2:$nmodes))
 end
