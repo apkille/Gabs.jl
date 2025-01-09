955d364264bf8dba0a8042e8e995f52bf6ab5045
Andrew Kille <68079167+apkille@users.noreply.github.com>
2025-01-09 18:16:43 -0500

    Add symplectic polar decomposition (#28)
    * add polar decomp
    
    * add polar tests
    
    * export `polar` and fix tests

--- src/Gabs.jl
+++ src/Gabs.jl
@@ -7,8 +7,8 @@ using LinearAlgebra: I, det, mul!, diagm, diag, qr, eigvals
 
 import QuantumInterface: StateVector, AbstractOperator, apply!, tensor, ⊗, directsum, ⊕
 
-import SymplecticFactorizations: williamson, Williamson, randsymplectic, symplecticform, issymplectic
-using SymplecticFactorizations: williamson, Williamson, BlockForm, PairForm
+import SymplecticFactorizations: williamson, Williamson, polar, Polar, randsymplectic, symplecticform, issymplectic
+using SymplecticFactorizations: williamson, Williamson, polar, Polar, BlockForm, PairForm
 
 export 
     # types
@@ -29,7 +29,7 @@ export
     # symplectic form and checks
     symplecticform, issymplectic, isgaussian, sympspectrum,
     # factorizations
-    williamson, Williamson,
+    williamson, Williamson, polar, Polar,
     # metrics
     purity
 
