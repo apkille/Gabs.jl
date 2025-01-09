86d25deaecc2b2e04f14f7539475a278a526a89a
apkille <killeandrew@gmail.com>
2025-01-09 14:22:33 -0500

    add polar tests

--- test/test_factorizations.jl
+++ test/test_factorizations.jl
@@ -24,4 +24,30 @@
         @test issymplectic(qpairbasis, S_pair, atol = 1e-5)
         @test issymplectic(qblockbasis, S_block, atol = 1e-5)
     end
+
+    @testset "polar" begin
+        nmodes = rand(1:5)
+        qpairbasis = QuadPairBasis(nmodes)
+        qblockbasis = QuadBlockBasis(nmodes)
+        
+        uni_pair = randunitary(qpairbasis)
+        uni_block = randunitary(qblockbasis)
+
+        F_pair = polar(uni_pair)
+        O_pair, P_pair = F_pair
+        F_block = williamson(uni_block)
+        O_block, P_block = F_block
+
+        @test O_pair == F_pair.O && P_pair == F_pair.P
+        @test O_block == F_block.O && P_block == F_block.P
+
+        @test isapprox(O_pair * P_pair, uni_pair.symplectic, atol = 1e-5)
+        @test isapprox(O_block * P_block, uni_block.symplectic, atol = 1e-5)
+
+        @test issymplectic(qpairbasis, O_pair, atol = 1e-5) & issymplectic(qblockbasis, O_block, atol = 1e-5)
+        @test issymplectic(qpairbasis, P_pair, atol = 1e-5) & issymplectic(qblockbasis, P_block, atol = 1e-5)
+        @test isapprox(inv(O_pair), transpose(O_pair), atol = 1e-5) && isapprox(inv(O_block), transpose(O_block), atol = 1e-5)
+        @test isapprox(P_block, transpose(P_block), atol = 1e-5) && all(i > 0 for i in eigvals(P_block))
+        @test isapprox(P_pair, transpose(P_pair), atol = 1e-5) && all(i > 0 for i in eigvals(P_pair))
+    end
 end
