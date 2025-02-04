58a8a6c8b1c8523e0ffb45151a3280c29aa11661
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-02-04 17:10:02 +0500

    resolve #43 Validate and add docs for Gaussian objects containing symbolic variables

--- test/test_states.jl
+++ test/test_states.jl
@@ -1,5 +1,6 @@
 @testitem "States" begin
     using Gabs
+    using Symbolics
     using StaticArrays
     using LinearAlgebra: det
 
---
+++ test/test_symbolic_states.jl
@@ -0,0 +1,46 @@
+@testitem "Symbolic States" begin
+    using Gabs
+    using Symbolics
+    using StaticArrays
+    using LinearAlgebra: det
+
+    nmodes = rand(1:5)
+    qpairbasis = QuadPairBasis(nmodes)
+    qblockbasis = QuadBlockBasis(nmodes)
+    @testset " Symbolic Partial Trace of Beamsplitter-EPR State" begin
+        @variables r θ τ
+        b = QuadBlockBasis(2)
+        st = eprstate(b, r, θ)
+        op = beamsplitter(b, τ)
+        newst = ptrace(op * st, 1)
+        expected_mean = zeros(2)
+        τ_sqrt = sqrt(τ)
+        one_minus_τ_sqrt = sqrt(1 - τ)
+        expected_covariance = [
+            (0.5cosh(2r) * one_minus_τ_sqrt - 0.5cos(θ) * sinh(2r) * τ_sqrt) * one_minus_τ_sqrt +
+            (0.5τ_sqrt * cosh(2r) - 0.5cos(θ) * sinh(2r) * one_minus_τ_sqrt) * τ_sqrt  -sinh(2r) * sin(θ) * τ_sqrt * one_minus_τ_sqrt;
+            -sinh(2r) * sin(θ) * τ_sqrt * one_minus_τ_sqrt  (0.5cosh(2r) * one_minus_τ_sqrt + 0.5cos(θ) * sinh(2r) * τ_sqrt) * one_minus_τ_sqrt +
+            (0.5τ_sqrt * cosh(2r) + 0.5cos(θ) * sinh(2r) * one_minus_τ_sqrt) * τ_sqrt
+        ]
+        @test newst isa GaussianState
+        @test newst.mean == expected_mean
+        @test all(isequal.(newst.covar, expected_covariance))
+    end
+
+    @testset "EPR states with Symbolics" begin
+        @variables r θ
+        nmodes = rand(1:5)
+        @variables rs[1:nmodes] thetas[1:nmodes]
+        qpairbasis = QuadPairBasis(nmodes)
+        qblockbasis = QuadBlockBasis(nmodes)
+        state = eprstate(2 * qpairbasis, r, θ)
+        @test state isa GaussianState
+        @test eprstate(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, 2*qpairbasis, r, θ) isa GaussianState
+        @test iszero(simplify(eprstate(2*qblockbasis, r, θ).covar - changebasis(QuadBlockBasis, state).covar))
+        @test iszero(simplify(eprstate(2*qblockbasis, r, θ).mean - changebasis(QuadBlockBasis, state).mean))
+        state_pair = eprstate(2*qpairbasis, collect(rs), collect(thetas))
+        state_block = eprstate(2*qblockbasis, collect(rs), collect(thetas))
+        @test iszero(simplify(state_block.covar - changebasis(QuadBlockBasis, state_pair).covar))
+        @test iszero(simplify(state_block.mean - changebasis(QuadBlockBasis, state_pair).mean))
+    end
+end
