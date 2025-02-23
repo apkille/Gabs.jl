f1525fe336999fd528f2b8bbf613384d26445c42
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-02-23 21:53:23 +0500

    improvements

--- src/Gabs.jl
+++ src/Gabs.jl
@@ -21,7 +21,7 @@ export
     vacuumstate, thermalstate, coherentstate, squeezedstate, eprstate,
     # predefined Gaussian channels
     displace, squeeze, twosqueeze, phaseshift, beamsplitter,
-    attenuator, amplifier, twosumgate,
+    attenuator, amplifier, sum_gate,
     # random objects
     randstate, randunitary, randchannel, randsymplectic,
     # wigner functions
