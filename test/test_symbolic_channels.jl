4f7dd50cb1826eb904417acc508df8b1644549e7
apkille <killeandrew@gmail.com>
2025-02-27 04:28:40 -0500

    clean up tests

--- test/test_symbolic_channels.jl
+++ test/test_symbolic_channels.jl
@@ -41,48 +41,38 @@
     end
 
     @testset "Symbolic tensor products" begin
-        @variables alpha1 alpha2 theta
-        noise_ds = [noise zeros(2*nmodes,2*nmodes); zeros(2*nmodes,2*nmodes) noise]
-        d1 = displace(qpairbasis, alpha1, noise)
-        d2 = displace(qpairbasis, alpha2, noise)
-        ds = tensor(d1, d2)
-        @test ds isa GaussianChannel
-        @test isapprox(ds, d1 ⊗ d2)
-        @test isapprox(ds, d1 ⊗ d2, atol = 1e-10)
-        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1, d2) isa GaussianChannel
-        p = phaseshift(qpairbasis, theta, noise)
-        @test isapprox(tensor(tensor(p, d1), d2), p ⊗ d1 ⊗ d2)
-        p_block = phaseshift(qblockbasis, theta, noise)
-        p_blocks = phaseshift(2*qblockbasis, repeat([theta], 2*nmodes), noise_ds)
-        @test isapprox(p_block ⊗ p_block, p_blocks)
-        @variables alphas1[1:nmodes] alphas2[1:nmodes] thetas[1:nmodes]
-        alphas1_vec = collect(alphas1)
-        alphas2_vec = collect(alphas2)
-        thetas_vec = collect(thetas)
-        d1 = displace(qpairbasis, alphas1_vec, noise)
-        d2 = displace(qpairbasis, alphas2_vec, noise)
-        ds = tensor(d1, d2)
-        @test ds isa GaussianChannel
-        @test isapprox(ds, d1 ⊗ d2)
-        @test isapprox(ds, d1 ⊗ d2, atol = 1e-10)
-        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1, d2) isa GaussianChannel
-        p = phaseshift(qpairbasis, thetas_vec, noise)
-        @test isapprox(tensor(tensor(p, d1), d2), p ⊗ d1 ⊗ d2)
+        noise_ds = [noise zeros(Num, 2*nmodes, 2*nmodes); zeros(Num, 2*nmodes, 2*nmodes) noise]
+
+        d1_pair = displace(qpairbasis, a, noise)
+        d2_pair = displace(qpairbasis, b, noise)
+        ds_pair = tensor(d1_pair, d2_pair)
+        @test ds_pair isa GaussianChannel
+        @test isapprox(ds_pair, d1_pair ⊗ d2_pair)
+        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1_pair, d2_pair) isa GaussianChannel
+        p_pair = phaseshift(qpairbasis, a, noise)
+        @test isapprox(tensor(tensor(p_pair, d1_pair), d2_pair), p_pair ⊗ d1_pair ⊗ d2_pair)
+        
+        d1_block = displace(qblockbasis, a, noise)
+        d2_block = displace(qblockbasis, b, noise)
+        ds_block = tensor(d1_block, d2_block)
+        @test ds_block isa GaussianChannel
+        @test isapprox(ds_block, d1_block ⊗ d2_block)
+        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1_block, d2_block) isa GaussianChannel
+        p_block = phaseshift(qblockbasis, a, noise)
+        @test isapprox(tensor(tensor(p_block, d1_block), d2_block), p_block ⊗ d1_block ⊗ d2_block)
     end
 
     @testset "Symbolic actions" begin
-        @variables alpha1 alpha2
-        d = displace(qpairbasis, alpha1, noise)
+        d = displace(qpairbasis, a, noise)
         v = vacuumstate(qpairbasis)
-        c = coherentstate(qpairbasis, alpha1)
-        @test isapprox(d * v, c)
-        @test isapprox(d * v, c, atol = 1e-10)
+        c = coherentstate(qpairbasis, b)
+        @test_broken isapprox(d * v, c) # due to equality checks for Num type in Symbolics.jl
         @test_broken isapprox(apply!(v, d), c)
         v1, v2 = vacuumstate(qpairbasis), vacuumstate(qpairbasis)
-        d1 = displace(qpairbasis, alpha1, noise)
-        d2 = displace(qpairbasis, alpha2, noise)
-        c1 = coherentstate(qpairbasis, alpha1)
-        c2 = coherentstate(qpairbasis, alpha2)
+        d1 = displace(qpairbasis, a, noise)
+        d2 = displace(qpairbasis, b, noise)
+        c1 = coherentstate(qpairbasis, a)
+        c2 = coherentstate(qpairbasis, b)
         @test isapprox((d1 ⊗ d2) * (v1 ⊗ v2), c1 ⊗ c2)
     end
 end
