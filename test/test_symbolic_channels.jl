a78bfe81d330b773ad36454982c3a9c87b264e5f
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-02-27 12:08:24 +0500

    add wonderful suggestions: symbolic tensor product and symbolic actions for channels

--- test/test_symbolic_channels.jl
+++ test/test_symbolic_channels.jl
@@ -39,4 +39,50 @@
             @test isapprox(op_block_multi, changebasis(QuadBlockBasis, op_pair_multi))
         end
     end
+
+    @testset "Symbolic tensor products" begin
+        @variables alpha1 alpha2 theta
+        noise_ds = [noise zeros(2*nmodes,2*nmodes); zeros(2*nmodes,2*nmodes) noise]
+        d1 = displace(qpairbasis, alpha1, noise)
+        d2 = displace(qpairbasis, alpha2, noise)
+        ds = tensor(d1, d2)
+        @test ds isa GaussianChannel
+        @test isapprox(ds, d1 ⊗ d2)
+        @test isapprox(ds, d1 ⊗ d2, atol = 1e-10)
+        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1, d2) isa GaussianChannel
+        p = phaseshift(qpairbasis, theta, noise)
+        @test isapprox(tensor(tensor(p, d1), d2), p ⊗ d1 ⊗ d2)
+        p_block = phaseshift(qblockbasis, theta, noise)
+        p_blocks = phaseshift(2*qblockbasis, repeat([theta], 2*nmodes), noise_ds)
+        @test isapprox(p_block ⊗ p_block, p_blocks)
+        @variables alphas1[1:nmodes] alphas2[1:nmodes] thetas[1:nmodes]
+        alphas1_vec = collect(alphas1)
+        alphas2_vec = collect(alphas2)
+        thetas_vec = collect(thetas)
+        d1 = displace(qpairbasis, alphas1_vec, noise)
+        d2 = displace(qpairbasis, alphas2_vec, noise)
+        ds = tensor(d1, d2)
+        @test ds isa GaussianChannel
+        @test isapprox(ds, d1 ⊗ d2)
+        @test isapprox(ds, d1 ⊗ d2, atol = 1e-10)
+        @test tensor(SVector{4*nmodes}, SMatrix{4*nmodes,4*nmodes}, d1, d2) isa GaussianChannel
+        p = phaseshift(qpairbasis, thetas_vec, noise)
+        @test isapprox(tensor(tensor(p, d1), d2), p ⊗ d1 ⊗ d2)
+    end
+
+    @testset "Symbolic actions" begin
+        @variables alpha1
+        d = displace(qpairbasis, alpha1, noise)
+        v = vacuumstate(qpairbasis)
+        c = coherentstate(qpairbasis, alpha1)
+        @test isapprox(d * v, c)
+        @test isapprox(d * v, c, atol = 1e-10)
+        @test_broken isapprox(apply!(v, d), c)
+        v1, v2 = vacuumstate(qpairbasis), vacuumstate(qpairbasis)
+        d1 = displace(qpairbasis, alpha1, noise)
+        d2 = displace(qpairbasis, alpha2, noise)
+        c1 = coherentstate(qpairbasis, alpha1)
+        c2 = coherentstate(qpairbasis, alpha2)
+        @test isapprox((d1 ⊗ d2) * (v1 ⊗ v2), c1 ⊗ c2)
+    end
 end
